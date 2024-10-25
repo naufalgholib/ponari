8d2f5269659d3eb04406864608f5140001a8d7fc
naufalgholib <naufalgholib@gmail.com>
2024-10-25 21:32:54 +0700

    edit port dockerscript

--- dockerscript.sh
+++ dockerscript.sh
@@ -7,8 +7,8 @@ echo ""
 docker build -t ponari:latest . 
 echo "===Running container==="
 echo ""
-docker run -p 3000:3000 --name ponari-website -d ponari > /dev/null 2>&1
+docker run -p 80:3000 --name ponari-website -d ponari > /dev/null 2>&1
 sleep 5
 echo "===Test access==="
 echo ""
-curl localhost:3000 | head
+curl localhost:80 | head
