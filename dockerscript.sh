834ae7f4ccd2b2c873e9cae44dd1aa205bd17bd1
naufalgholib <naufalgholib@gmail.com>
2024-10-26 11:28:16 +0700

    edit nama container untuk testing local

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,13 +1,13 @@
 echo "===Stop current docker container if there's any===" 
 echo ""
-docker stop ponari-website > /dev/null 2>&1 
-docker rm ponari-website > /dev/null 2>&1
+docker stop ponari-frontend > /dev/null 2>&1 
+docker rm ponari-frontend > /dev/null 2>&1
 echo "===Building docker images==="
 echo ""
 docker build -t ponari:latest . 
 echo "===Running container==="
 echo ""
-docker run -p 80:3000 --name ponari-website -d ponari > /dev/null 2>&1
+docker run -p 80:3000 --name ponari-frontend -d ponari > /dev/null 2>&1
 sleep 5
 echo "===Test access==="
 echo ""
